96acfd1925ef5d63f90b6b210ab26da1d9a8e3b4
jacquelinemcc <mccarrja@tcd.ie>
2021-04-19 21:45:38 +0100

    add name to test

---
+++ ADSGroupAssignment/src/testGit.java
@@ -0,0 +1,11 @@
+
+public class testGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//add your name to string and commit to github to test
+		String testString = "jacqueline";
+	}
+
+}
